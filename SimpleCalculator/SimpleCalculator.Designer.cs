449a1a8daf2614efe812e85b6f8d7ff73a9ae3f6
AbramGirgis <girgis.abram@gmail.com>
2022-07-10 17:40:00 -0400

    completed

--- SimpleCalculator/SimpleCalculator.Designer.cs
+++ SimpleCalculator/SimpleCalculator.Designer.cs
@@ -31,6 +31,11 @@
             this.buttonAdd = new System.Windows.Forms.Button();
             this.buttonSubtraction = new System.Windows.Forms.Button();
             this.buttonExit = new System.Windows.Forms.Button();
+            this.labelNumber1 = new System.Windows.Forms.Label();
+            this.labelNumber2 = new System.Windows.Forms.Label();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.textBoxNumber1 = new System.Windows.Forms.TextBox();
+            this.textBoxNumber2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // buttonAdd
@@ -38,42 +43,93 @@
             this.buttonAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonAdd.Location = new System.Drawing.Point(735, 106);
             this.buttonAdd.Name = "buttonAdd";
-            this.buttonAdd.Size = new System.Drawing.Size(188, 78);
+            this.buttonAdd.Size = new System.Drawing.Size(199, 78);
             this.buttonAdd.TabIndex = 0;
             this.buttonAdd.Text = "Add";
             this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
             // 
             // buttonSubtraction
             // 
             this.buttonSubtraction.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonSubtraction.Location = new System.Drawing.Point(735, 240);
             this.buttonSubtraction.Name = "buttonSubtraction";
-            this.buttonSubtraction.Size = new System.Drawing.Size(188, 78);
+            this.buttonSubtraction.Size = new System.Drawing.Size(199, 78);
             this.buttonSubtraction.TabIndex = 1;
             this.buttonSubtraction.Text = "Subtract";
             this.buttonSubtraction.UseVisualStyleBackColor = true;
+            this.buttonSubtraction.Click += new System.EventHandler(this.buttonSubtraction_Click);
             // 
             // buttonExit
             // 
             this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.85714F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonExit.Location = new System.Drawing.Point(735, 478);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(188, 78);
+            this.buttonExit.Size = new System.Drawing.Size(199, 78);
             this.buttonExit.TabIndex = 2;
             this.buttonExit.Text = "Exit";
             this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelNumber1
+            // 
+            this.labelNumber1.AutoSize = true;
+            this.labelNumber1.Location = new System.Drawing.Point(103, 106);
+            this.labelNumber1.Name = "labelNumber1";
+            this.labelNumber1.Size = new System.Drawing.Size(97, 25);
+            this.labelNumber1.TabIndex = 3;
+            this.labelNumber1.Text = "Number 1";
+            // 
+            // labelNumber2
+            // 
+            this.labelNumber2.AutoSize = true;
+            this.labelNumber2.Location = new System.Drawing.Point(103, 240);
+            this.labelNumber2.Name = "labelNumber2";
+            this.labelNumber2.Size = new System.Drawing.Size(97, 25);
+            this.labelNumber2.TabIndex = 4;
+            this.labelNumber2.Text = "Number 2";
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.14286F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(103, 478);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(278, 38);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.Text = "Operation Result";
+            // 
+            // textBoxNumber1
+            // 
+            this.textBoxNumber1.Location = new System.Drawing.Point(108, 154);
+            this.textBoxNumber1.Name = "textBoxNumber1";
+            this.textBoxNumber1.Size = new System.Drawing.Size(308, 29);
+            this.textBoxNumber1.TabIndex = 6;
+            // 
+            // textBoxNumber2
+            // 
+            this.textBoxNumber2.Location = new System.Drawing.Point(108, 289);
+            this.textBoxNumber2.Name = "textBoxNumber2";
+            this.textBoxNumber2.Size = new System.Drawing.Size(308, 29);
+            this.textBoxNumber2.TabIndex = 7;
             // 
             // SimpleCalculator
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1087, 654);
+            this.Controls.Add(this.textBoxNumber2);
+            this.Controls.Add(this.textBoxNumber1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.labelNumber2);
+            this.Controls.Add(this.labelNumber1);
             this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.buttonSubtraction);
             this.Controls.Add(this.buttonAdd);
             this.Name = "SimpleCalculator";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,6 +138,11 @@
         private System.Windows.Forms.Button buttonAdd;
         private System.Windows.Forms.Button buttonSubtraction;
         private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelNumber1;
+        private System.Windows.Forms.Label labelNumber2;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.TextBox textBoxNumber1;
+        private System.Windows.Forms.TextBox textBoxNumber2;
     }
 }
 
--- SimpleCalculator/SimpleCalculator.cs
+++ SimpleCalculator/SimpleCalculator.cs
@@ -16,5 +16,63 @@ namespace SimpleCalculator
         {
             InitializeComponent();
         }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult answer = MessageBox.Show("Are you sure you want to exit?", "Confirmation",
+                                                    MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if(answer == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            // simple basic validation
+            if (textBoxNumber1.Text == "")
+            {
+                MessageBox.Show("Number 1 is required", "Missing value");
+                textBoxNumber1.Focus();
+            }
+            else if (textBoxNumber2.Text == "")
+            {
+                MessageBox.Show("Number 2 is required", "Missing value");
+                textBoxNumber2.Focus();
+            }
+            else
+            {
+                labelResult.Text = "The sum is: " + (Convert.ToInt32(textBoxNumber1.Text) + 
+                                                     Convert.ToInt32(textBoxNumber2.Text));
+            }
+        }
+
+        private void buttonSubtraction_Click(object sender, EventArgs e)
+        {
+            if (textBoxNumber1.Text == "")
+            {
+                MessageBox.Show("Number 1 is required", "Missing value");
+                textBoxNumber1.Focus();
+            }
+            else if (textBoxNumber2.Text == "")
+            {
+                MessageBox.Show("Number 2 is required", "Missing value");
+                textBoxNumber2.Focus();
+            }
+            else
+            {
+                int result = (Convert.ToInt32(textBoxNumber1.Text) - Convert.ToInt32(textBoxNumber2.Text));
+                if(result < 0)
+                {
+                    MessageBox.Show("Number 1 is less than number 2. Please swap the numbers!", "Negative value");
+                    textBoxNumber1.Focus();
+                }
+                else
+                {
+                    labelResult.Text = "The difference is: " + result;
+                    MessageBox.Show("The difference is: " + result, "Difference");
+                }
+            }
+        }
     }
 }
